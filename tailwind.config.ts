392e2b79eb672d7125f194ffaed59eb43dc4c0ed
Diego Gonzalez King <dgkbsas@gmail.com>
2026-03-09 11:10:35 -0300

    fix: use design token for app background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ export default {
 		},
 		extend: {
 			colors: {
+				'app-background': 'var(--app-background)',
 				/* Shadcn semantic tokens */
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
